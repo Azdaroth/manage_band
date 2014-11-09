7a156acbebb1ac17ed23607367bdb98fae50385c
Karol Galanciak <karol.galanciak@gmail.com>
2014-11-09 14:15:48 +0100

    refactoring of registration process - now the controller that inherits from DeviseTokenAuth::RegistrationsController is not that bad
    remove not needed method from RegistrationsController

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,18 +1,12 @@
 class RegistrationsController < DeviseTokenAuth::RegistrationsController
 
-  # The code is mostly awful... but that's not my code, pasted from gem's code
-  # but I need to extend it and don't want to mess further
-  # refactored a bit
+  # The code was mostly awful here... but it was not my idea, just pasted from gem's code
+  # refactored a lot of parts to make it managable and extensible
+
   def create
-    @resource = setup_resource
-    return confirm_success_url_param_missing unless confirm_success_url_param_missing?
-    return email_already_taken if email_taken?
+    form = User::RegistrationForm.new(setup_resource)
 
-    form = User::RegistrationForm.new(@resource)
-
-    # override email confirmation, must be sent manually from ctrl
-    @resource.skip_confirmation_notification!
-    if form.persist(sign_up_params)
+    if form.persist(params)
       user = form.model
       if user.confirmed?
         # email auth has been bypassed, authenticate user
@@ -24,7 +18,7 @@ class RegistrationsController < DeviseTokenAuth::RegistrationsController
       end
       success_response(user)
     else
-      resource_invalid(form.model)
+      resource_invalid(form)
     end
   end
 
@@ -34,32 +28,9 @@ class RegistrationsController < DeviseTokenAuth::RegistrationsController
       resource = resource_class.new(sign_up_params.except(:band_name))
       resource.uid = sign_up_params[:email]
       resource.provider = "email"
+      # override email confirmation, must be sent manually from ctrl
+      resource.skip_confirmation_notification!
       resource
-    end
-
-    def confirm_success_url_param_missing
-      render json: {
-          status: 'error',
-          data:   @resource,
-          errors: ["Missing `confirm_success_url` param."]
-      }, status: 403
-    end
-
-    def confirm_success_url_param_missing?
-      params[:confirm_success_url]
-    end
-
-    def email_taken?
-      User.where(email: sign_up_params[:email]).exists?
-    end
-
-    def email_already_taken
-      clean_up_passwords @resource
-      render json: {
-        status: 'error',
-        data:   @resource,
-        errors: ["An account already exists for #{@resource.email}"]
-      }, status: 403
     end
 
     def send_confirmation_instructions(resource)
--- app/forms/user/registration_form.rb
+++ app/forms/user/registration_form.rb
@@ -6,10 +6,12 @@ class User::RegistrationForm < Reform::Form
   property :password
   property :password_confirmation
   property :band_name, empty: true
+  property :confirm_success_url, empty: true
 
   validates :email, presence: true, email: true, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, confirmation: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 9 }
   validates :band_name, presence: true
+  validates :confirm_success_url, presence: true
 
   def band_name=(name)
     band = Band.where(name: name).first_or_initialize
